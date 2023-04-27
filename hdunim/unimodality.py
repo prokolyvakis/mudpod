a2f085383ef7485a942dff26ef0d71d025d394da
Prodromos Kolyvakis <prokolyvakis@gmail.com>
2023-04-27 16:54:30 +0300

    feat: add unimodality hypothesis testing

---
+++ hdunim/unimodality.py
@@ -0,0 +1,92 @@
+"""Definition of unimodality tests."""
+from dataclasses import dataclass
+
+from diptest import diptest
+from loguru import logger
+from mpire import WorkerPool
+import numpy as np
+
+from hdunim.projections import IdentityProjector
+from hdunim.projections import View
+
+
+@dataclass
+class UnimodalityTester:
+    """A wrapper class for checking the unimodality of a dataset using views."""
+
+    view: View
+    # The view over which the dip test will be performed.
+
+    pval: float
+    # A float in the interval (0, 1) indicating the p_value.
+
+    boot_pval: bool = False
+    # A boolean flag indicating whether to boostrap the dip value.
+
+    def __post_init__(self):
+        assert 0. < self.pval < 1., f'The p_value {self.pval} does not lie in (0, 1).'
+
+    def test(self, x: np.ndarray) -> bool:
+        """A test that assesses the \alpha-unimodality of a dataset using a random view.
+
+        Args:
+        x: α 2D numpy array with the first dimension being the number of different
+                datapoints and the second being the features' size.
+        Returns:
+            A boolean indicating whether the data follow a \alpha-unimodal distribution.
+        """
+        ds = self.view.distances(x)
+
+        _, pv = diptest(ds, boot_pval=self.boot_pval)
+
+        return pv > self.pval
+
+
+@dataclass
+class MonteCarloUnimodalityTester:
+    """
+    A wrapper class for checking the unimodality of a dataset using Monte Carlo views.
+    """
+
+    tester: UnimodalityTester
+    # A unimodality tester
+
+    workers_num: int
+    # The number of workers
+
+    sim_num: int
+    # The number of Monte Carlo simulations.
+
+    def __post_init__(self):
+        if self.workers_num < 0:
+            raise ValueError('The number of works must be non-negative!')
+        if self.tester.view.projector == IdentityProjector:
+            raise ValueError('Cannot perform Monte Carlo simulations '
+                             'without random projections!')
+        if self.sim_num == 1:
+            raise ValueError("The simulations' number must be greater than 1!")
+
+    def test(self, x: np.ndarray) -> bool:
+        """A test that assesses the \alpha-unimodality of a dataset using a random view.
+
+        Args:
+        x: α 2D numpy array with the first dimension being the number of different
+                datapoints and the second being the features' size.
+        Returns:
+            A boolean indicating whether the data follow a \alpha-unimodal distribution.
+        """
+        def generator(n):
+            i = 0
+            while i < n:
+                yield x
+                i += 1
+
+        func = self.tester.test
+        with WorkerPool(n_jobs=self.workers_num) as pool:
+            tests = pool.map(func, generator(self.sim_num))
+
+        logger.debug(f'The result of the Monte Carlo simulations is: {tests}')
+
+        pv = (1. * np.count_nonzero(tests)) / self.sim_num
+
+        return pv
---
+++ tests/test_unimodality.py
@@ -0,0 +1,62 @@
+"""Tests written for the unimodality module."""
+import pytest
+
+import numpy as np
+from sklearn.datasets import make_blobs
+
+from hdunim.misc import set_seed
+from hdunim.projections import JohnsonLindenstrauss
+from hdunim.projections import IdentityProjector
+from hdunim.observer import PercentileObserver
+from hdunim.projections import View
+from hdunim.unimodality import UnimodalityTester
+from hdunim.unimodality import MonteCarloUnimodalityTester
+
+set_seed(42)
+
+
+@pytest.mark.parametrize("projector", [JohnsonLindenstrauss])
+@pytest.mark.parametrize("observer", [PercentileObserver(0.95)])
+@pytest.mark.parametrize("n_features", [200, 400])
+def test_unimodality_tester(projector, observer, n_features) -> None:
+    """Test that the unimodality tester works properly."""
+
+    v = View(projector, observer)
+
+    x, _ = make_blobs(
+        n_samples=1000,
+        n_features=n_features,
+        centers=2,
+        cluster_std=1.,
+        random_state=42
+    )
+
+    t = UnimodalityTester(v, 0.05)
+
+    assert not t.test(x)
+
+
+@pytest.mark.parametrize("projector", [JohnsonLindenstrauss])
+@pytest.mark.parametrize("observer", [PercentileObserver(0.95)])
+@pytest.mark.parametrize("n_features", [200, 400])
+@pytest.mark.parametrize("workers_num", [0, 1, 10])
+@pytest.mark.parametrize("sim_num", [10, 20])
+def test_monte_carlo_unimodality_tester(
+        projector, observer, n_features, workers_num, sim_num) -> None:
+    """Test that the Monte Carlo unimodality tester works properly."""
+
+    v = View(projector, observer)
+
+    x, _ = make_blobs(
+        n_samples=1000,
+        n_features=n_features,
+        centers=2,
+        cluster_std=1.,
+        random_state=42
+    )
+
+    t = UnimodalityTester(v, 0.05)
+
+    mct = MonteCarloUnimodalityTester(t, workers_num, sim_num)
+
+    assert not mct.test(x)
