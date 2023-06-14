3dea74a66278dd93545f4c60a0db9672679ab061
Prodromos Kolyvakis <prokolyvakis@gmail.com>
2023-06-14 13:24:19 +0000

    feat: add `docopt` for argument parsing in experiments

--- experiments/synthetic/unimodality.py
+++ experiments/synthetic/unimodality.py
@@ -1,6 +1,19 @@
-"""Unimodality hypothesis testing experiments with synthetic datasets.s."""
-import sys
+"""Unimodality hypothesis testing experiments with synthetic datasets.
 
+Usage:
+  unimodality.py <d> <pj> <pv> <sims> [--samples=<s> --noise=<n> --seed=<sd>]
+  unimodality.py -h | --help
+
+Options:
+  -h --help         Show this screen.
+  --samples=<s>     The number of samples [default: 200].
+  --noise=<n>       The standard deviation inside the clusters [default: 0].
+  --seed=<sd>       The seed [default: 42].
+"""
+import sys
+from typing import Callable
+
+from docopt import docopt
 from loguru import logger
 from sklearn.datasets import make_blobs
 from sklearn.datasets import make_circles
@@ -9,7 +22,7 @@ from sklearn.datasets import make_swiss_roll
 from sklearn.datasets import load_digits
 from sklearn.datasets import load_iris
 
-
+from experiments.common import plot_clustered_data
 from hdunim.misc import set_seed
 from hdunim.projections import IdentityProjector
 from hdunim.projections import JohnsonLindenstrauss
@@ -18,23 +31,47 @@ from hdunim.projections import View
 from hdunim.unimodality import UnimodalityTest
 from hdunim.unimodality import MonteCarloUnimodalityTest
 
-SEED = 42
-
-set_seed(SEED)
-
 logger.remove()
 # add a new handler with level set to INFO
 logger.add(sys.stderr, level="INFO")
 
 
-if __name__ == "__main__":
-    v = View(JohnsonLindenstrauss, PercentileObserver(0.99))
-    t = UnimodalityTest(v, 0.01)
-    mct = MonteCarloUnimodalityTest(t, sim_num=1000, workers_num=10)
+def get_dataset(name: str) -> Callable:
+    if name == 'circles':
+        return make_circles
+    elif name == 'moons':
+        return make_moons
+    elif name == 'swiss_roll':
+        return make_swiss_roll
+    else:
+        msg = f'Unknown dataset name: {name}'
+        logger.error(msg)
+        raise ValueError(msg)
 
-    data_func = make_swiss_roll
-    n_samples = 200
-    noise = 0
+
+if __name__ == "__main__":
+    arguments = docopt(__doc__)
+    logger.info(f'The parameters are: {arguments}.')
+
+    SEED = int(arguments['--seed'])
+    set_seed(SEED)
+
+    pt = str(arguments['<pj>'])
+    p = JohnsonLindenstrauss if pt == 'jl' else IdentityProjector
+    v = View(p, PercentileObserver(0.99))
+    t = UnimodalityTest(v, float(arguments['<pv>']))
+    mct = MonteCarloUnimodalityTest(
+        t,
+        sim_num=int(arguments['<sims>']),
+        workers_num=10
+    )
+
+    data_func = get_dataset(str(arguments['<d>']))
+    n_samples = int(arguments['--samples'])
+    noise = float(arguments['--noise'])
     x, y = data_func(n_samples=n_samples, noise=noise, random_state=SEED)
 
-    logger.info(f'The statistical test says that {mct.test(x)}')
+    tr = 'unimodal' if mct.test(x) else 'multimodal'
+    logger.info(f'The statistical test says {tr}')
+
+    plot_clustered_data(x, y)
